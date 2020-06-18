4b732ea4af03ec391b73750ed9b7ace97788adad
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-06-18 15:22:27 -0700

    removed django_heroku from settings to test if deploy

--- memento/settings.py
+++ memento/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,6 +133,3 @@ STATIC_FILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, 'media')
 MEDIA_URL = '/media/'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
